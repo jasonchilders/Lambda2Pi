e1de27d807c54ab38064533602f2f2f88551038f
jasonchilders <jason.childers@gmail.com>
2015-03-13 01:40:27 -0700

    Added debug statements, and formatted output.

--- Lambda2Pi.py
+++ Lambda2Pi.py
@@ -4,23 +4,25 @@ __author__ = 'Jason Childers'
 
 
 import argparse
-from LambdaTerm import LambdaTerm
 
+from LambdaTerm import LambdaTerm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("lambdaterm", type=str, help="a lambda term [ex: 'lambda x: x(42)']")
 parser.add_argument("-d", "--debug", help="show debug info", action="store_true")
 args = parser.parse_args()
 if args.debug:
-    print('debug is turned on');
+    LambdaTerm.debug = True
+    if args.debug: print('(debug) debug is turned on')
 
 
 def main(args):
     if (args is not None):
-        print(args)
-        print('args: ' + str(args))
+        if args.debug: print('(debug) args: ' + str(args))
         lambdaTerm = LambdaTerm(args.lambdaterm)
-        print(lambdaTerm.astTerms)
+        print('\n>>> lambda term: ' + lambdaTerm.term + '\n')
+        if args.debug: print('(debug) astTerms: ' + str(lambdaTerm.astTerms))
+        print('>>> pi-calculus process: ' + lambdaTerm.piProcessExpression + '\n')
     else:
         print('no args');
 
